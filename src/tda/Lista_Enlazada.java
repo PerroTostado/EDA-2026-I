e39533c38e7af672ee694afffdbdce795031970c
PerroTostado <116856290+PerroTostado@users.noreply.github.com>
2026-03-05 10:26:58 -0500

    Add files via upload

---
+++ src/tda/Lista_Enlazada.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tda;
+
+/**
+ *
+ * @author CENTIC
+ */
+public class Lista_Enlazada {
+
+    public static void main(String[] args) {
+        Lista L = new Lista();
+        System.out.println(L);
+    }
+    
+}
---
+++ src/tda/Nodo.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tda;
+
+/**
+ *
+ * @author CENTIC
+ */
+public class Nodo {
+    public int dato;
+    public Nodo siguiente = null;
+    
+    public Nodo(int dato){
+        this.dato = dato;
+    }
+    
+    
+}
